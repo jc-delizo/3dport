684d55f7e119de7270179b03f114bd063250060e
JC Delizo <mrjcdelizo@gmail.com>
2023-04-17 21:40:50 +0800

    Update NavBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
           }}
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
-          <p className="text-white text-[18px] font-bold cursor-pointer flex">&nbsp; JC Delizo &nbsp; <span className="sm:block hidden">| Web Developer</span></p>
+          <p className="text-white text-[18px] font-bold cursor-pointer flex">&nbsp; <span className="sm:block hidden"> Web Developer</span></p>
           
         </Link>
         <ul className='list-none hidden sm:flex flex-row gap-10'>
